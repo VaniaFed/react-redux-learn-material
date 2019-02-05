5425ced3a24f77c5bc80ddeb7c940e9feaaf4900
Ivan <vaniafed@nicePC.Dlink>
2019-02-05 08:57:56 +0300

    I added Recipe and put it on the Menu

--- cp 5/src/public/js/index.jsx
+++ cp 5/src/public/js/index.jsx
@@ -39,16 +39,33 @@ const data = [
   }
 ];
 
-// const Recipe = props => (
+const Recipe = ({ name, ingredients, steps }) => (
+  <section id={ name.toLowerCase().replace(/ /g, '-') }>
+    <h1>{ name }</h1>
+    <ul className="ingredients">
+      {ingredients.map((ingredient, i) =>
+        <li key={ i }>{ ingredient.name }</li>
+      )}
+    </ul>
+    <section className="instructions">
+      <h2>Cooking Instructions</h2>
+      {steps.map((step, i) => 
+        <p key={ i }>{ step }</p>
+      )}
+    </section>
+  </section>
+);
 
-// );
-
-const Menu = props => (
+const Menu = ({ title, recipes}) => (
   <article>
     <header>
-      <h1></h1>
+      <h1>{ title }</h1>
     </header>
-    <div className="recipes"></div>
+    <div className="recipes">
+      {recipes.map((recipe, i) => 
+        <Recipe key={i} {...recipe} />
+      )}
+    </div>
   </article>
 );
 
