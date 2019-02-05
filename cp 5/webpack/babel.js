505b144015c1f4871b785a3204328249618595ac
Ivan <vaniafed@nicePC.Dlink>
2019-02-05 07:41:21 +0300

    add data to cp 5

---
+++ cp 5/webpack/babel.js
@@ -0,0 +1,13 @@
+module.exports = {
+  test: /.js|jsx$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+      ],
+    },
+  },
+};
---
+++ cp 5/webpack/sass.js
@@ -0,0 +1,8 @@
+module.exports = {
+  test: /\.sass$/,
+  use: [
+    'style-loader',
+    'css-loader',
+    'sass-loader',
+  ],
+};
