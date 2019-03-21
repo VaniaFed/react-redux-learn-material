b9bb9ccc2a746e7f2ce14ec0791cdc7c7c7960eb
VaniaFed <ztankist@gmail.com>
2019-03-21 10:26:34 +0300

    added reducer (color)

--- cp 8/state/src/public/js/reducers.js
+++ cp 8/state/src/public/js/reducers.js
@@ -20,6 +20,32 @@ export const color =  (state={}, action) => {
   }
 }
 
+export const colors =  (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_COLOR:
+      return {
+        ...state,
+        color({}, action)
+      }
+    case C.RATE_COLOR:
+      return state.map(
+        currentColor => color(currentColor, action)
+      )
+    case C.REMOVE_COLOR:
+      return state.filter(
+        currentColor => currentColor.id !== action.id
+      )
+    default:
+        return state
+  }
+}
+
+export const sort =  (state='SORTED_BY_DATE', action) => {
+  return ''
+}
+
+
+
 const actionAdd = {
   type: C.ADD_COLOR,
   id: '1',
@@ -38,11 +64,3 @@ const actionRate = {
 }
 const ratedColor = color(newColor, actionRate)
 console.log(ratedColor)
-
-export const colors =  (state=[], action) => {
-  return []
-}
-
-export const sort =  (state='SORTED_BY_DATE', action) => {
-  return ''
-}
